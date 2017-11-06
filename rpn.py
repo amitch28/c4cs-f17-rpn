3ac2ce610efc98b890032c8d529dfb8681745956
apmit <apmit@umich.edu>
2017-11-05 23:16:02 -0500

    added different colored output

--- rpn.py
+++ rpn.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import operator
+import colorama
+from colorama import init, Fore, Back
+import readline
 
 operators = {
     '+': operator.add,
@@ -22,16 +25,20 @@ def calculate(myarg):
 			arg1 = stack.pop()
 			result = function(arg1, arg2)
 			if result == 13:
-				print("Go Blue")	
+				print(Fore.BLUE + "Go Blue")	
 			stack.append(result)
+		print(Fore.YELLOW)
 		print(stack)
 	if len(stack) != 1:
 		raise TypeError("Too many parameters")
 	return stack.pop()
 
 def main():
+	colorama.init()
+
 	while True:
-		result = calculate(input("rpn calc> "))
+		result = calculate(input(Fore.RED + "rpn calc> "))
+		print(Fore.GREEN)
 		print("Result: ", result)
 
 if __name__ == '__main__':
